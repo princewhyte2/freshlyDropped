22055eae8ba37360ea496df45a9ba766e61c7b23
princewhyte2 <princewhyte02@gmail.com>
2021-07-29 11:46:42 +0100

    feature: onboarding screen built

---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Screen 2</Text>
+    </View>
+  );
+};
+
+export default Welcome;
